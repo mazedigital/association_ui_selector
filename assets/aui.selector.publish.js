0461109e21c230a23529636999776eaa179c961b
Nils Hörrmann <post@nilshoerrmann.de>
2014-06-04 14:56:04 +0200

    Setup Selectize with placeholder text and remove button

--- assets/aui.selector.publish.js
+++ assets/aui.selector.publish.js
@@ -1,12 +1,35 @@
 (function($, Symphony) {
 	'use strict';
 
+	Symphony.Language.add({
+		'Remove': false,
+		'Search and select': false
+	});
+
 	Symphony.Extensions.AssociationUISelector = function() {
 		var fields;
 
 		var init = function() {
-			fields = Symphony.Elements.contents.find('.field-selectbox_link');
-			fields.find('select').selectize();
+			fields = Symphony.Elements.contents.find('.field-selectbox_link').attr('data-ui', 'aui-selector');
+			fields.each(buildInterface);
+		};
+
+		var buildInterface = function() {
+			var select = $(this).find('select');
+
+			// Apply Selectize
+			select.selectize({
+				plugins: {
+					'remove_button': {
+						label : Symphony.Language.get('Remove'),
+						title : Symphony.Language.get('Remove'),
+						className : 'destructor'
+					}
+				}			
+			});
+
+			// Set placeholder text
+			select[0].selectize.$control_input.attr('placeholder', 'Search and select' + ' …');
 		};
 
 		// API
