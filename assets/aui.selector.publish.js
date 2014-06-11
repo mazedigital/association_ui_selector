d2de8281e72ef18606aa15137429f593efb8b8d4
Nils Hörrmann <post@nilshoerrmann.de>
2014-06-11 14:51:44 +0200

    Item layouts

--- assets/aui.selector.publish.js
+++ assets/aui.selector.publish.js
@@ -57,6 +57,9 @@
 				render: {
 					item: function(data, escape) {
 						return '<div class="item"><span>' + escape(data.text) + '</span></div>';
+					},
+					option: function(data, escape) {
+						return '<div class="option item"><span>' + escape(data.text) + '</span></div>';
 					}
 				},
 				load: function(query, callback) {
