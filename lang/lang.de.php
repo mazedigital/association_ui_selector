833035955008d5ec273189c37bed5bdb422ba369
Alexander Rutz <ar@animaux.de>
2014-08-13 17:40:47 +0200

    Add german translation
    including get »Search and select«

---
+++ lang/lang.de.php
@@ -0,0 +1,22 @@
+<?php
+
+	$about = array(
+		'name' => 'Deutsch',
+		'author' => array(
+			'name' => 'Alexander Rutz',
+			'email' => 'alex@animaux.de',
+			'website' => 'http://animaux.de'
+		),
+		'release-date' => '2014-08-13'
+	);
+
+	$dictionary = array(
+
+    'Remove' => 
+		'entfernen',
+		
+		'Search and select' => 
+		'suchen und auswählen'
+
+
+	);
