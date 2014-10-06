8d03d7c1d800cbdc58bebf864e1c8310b3088e05
Nils Hörrmann <post@nilshoerrmann.de>
2014-10-06 09:34:36 +0200

    Fix #38

--- content/content.get.php
+++ content/content.get.php
@@ -11,42 +11,45 @@ Class contentExtensionAssociation_ui_selectorGet extends JSONPage
         $field_ids = explode(',', General::sanitize($_GET['field_id']));
 
         $parent_section_id = EntryManager::fetchEntrySectionID($entry_id);
-        $parent_section = SectionManager::fetch($parent_section_id);
-        $parent_section_handle = $parent_section->get('handle');
 
-        // Fetch entry
-        $value = '';
-        if(!empty($field_ids[0])) {
-            $entry = EntryManager::fetch($entry_id);
+        if($parent_section_id) {
+            $parent_section = SectionManager::fetch($parent_section_id);
+            $parent_section_handle = $parent_section->get('handle');
 
-            foreach ($field_ids as $field_id) {
-                $field_data = $entry[0]->getData($field_id);
+            // Fetch entry
+            $value = '';
+            if(!empty($field_ids[0])) {
+                $entry = EntryManager::fetch($entry_id);
 
-                if (!empty($field_data)) {
-                    $field = FieldManager::fetch($field_id);
+                foreach ($field_ids as $field_id) {
+                    $field_data = $entry[0]->getData($field_id);
 
-                    if ($field instanceof ExportableField && in_array(ExportableField::UNFORMATTED, $field->getExportModes())) {
+                    if (!empty($field_data)) {
+                        $field = FieldManager::fetch($field_id);
 
-                        // Get unformatted value
-                        $value = $field->prepareExportValue($field_data, ExportableField::UNFORMATTED, $entry_id);
+                        if ($field instanceof ExportableField && in_array(ExportableField::UNFORMATTED, $field->getExportModes())) {
 
-                    } elseif ($field instanceof ExportableField && in_array(ExportableField::VALUE, $field->getExportModes())) {
+                            // Get unformatted value
+                            $value = $field->prepareExportValue($field_data, ExportableField::UNFORMATTED, $entry_id);
 
-                        // Get formatted value
-                        $value = $field->prepareExportValue($field_data, ExportableField::VALUE, $entry_id);
-                    } else {
+                        } elseif ($field instanceof ExportableField && in_array(ExportableField::VALUE, $field->getExportModes())) {
 
-                        // Get value from parameter pool
-                        $value = $field->getParameterPoolValue($field_data, $entry_id);
+                            // Get formatted value
+                            $value = $field->prepareExportValue($field_data, ExportableField::VALUE, $entry_id);
+                        } else {
+
+                            // Get value from parameter pool
+                            $value = $field->getParameterPoolValue($field_data, $entry_id);
+                        }
                     }
                 }
             }
-        }
 
-        // Set data
-        $this->_Result['entry']['value'] = $value;
-        $this->_Result['entry']['section'] = $parent_section_handle;
-        $this->_Result['entry']['link'] = APPLICATION_URL . '/publish/' . $parent_section_handle . '/edit/' . $entry_id . '/';
+            // Set data
+            $this->_Result['entry']['value'] = $value;
+            $this->_Result['entry']['section'] = $parent_section_handle;
+            $this->_Result['entry']['link'] = APPLICATION_URL . '/publish/' . $parent_section_handle . '/edit/' . $entry_id . '/';
+        }
 
         // Return results
         return $this->_Result;
