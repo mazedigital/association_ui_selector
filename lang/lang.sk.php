8bdad9b78ec1847b98d15c0b49b43f4b3fc5db9d
Juraj Kapsz <dev@jurajkapsz.sk>
2016-11-16 23:59:36 +0100

    Added Slovak translation.

---
+++ lang/lang.sk.php
@@ -0,0 +1,26 @@
+<?php
+
+	$about = array(
+		'name' => 'Slovensky',
+		'author' => array(
+			'name' => 'Juraj Kapsz',
+			'email' => 'hi@jurajkapsz.sk',
+			'website' => 'http://jurajkapsz.sk'
+		),
+		'release-date' => '2016-11-16'
+	);
+
+	/**
+	 * Association UI: Selector
+	 */
+	$dictionary = array(
+
+		// Missing
+
+    'Remove' =>
+		'Odstrániť',
+
+		'Search and select' =>
+		'Vyhľadať a vybrať'
+
+	);
